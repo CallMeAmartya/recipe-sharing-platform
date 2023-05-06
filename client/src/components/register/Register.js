09718f367bc233eac2a752af3edcd85ebee1f704
Amartya Sen <amartyasen00077@gmail.com>
2023-05-07 04:42:30 +0530

    1. Integrates react-bootstrap 2. Used bootstrap to render registation form

--- client/src/components/register/Register.js
+++ client/src/components/register/Register.js
@@ -1,29 +1,47 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { registerUser } from "../../api/api";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const Register = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  // const [name, setName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    const key = e.target.name;
+    const value = e.target.value;
+    setFormData({ ...formData, [key]: value });
+    console.log(formData);
+  };
 
   const navigate = useNavigate();
 
   const handleFormSubmit = async (e) => {
     e.preventDefault();
+    console.log(e);
 
-    if (password !== confirmPassword) {
+    if (formData["password"] !== formData["confirmPassword"]) {
       setError("Passwords do not match!");
       return;
     }
 
     const userData = {
-      name,
-      email,
-      password,
+      name: formData["name"],
+      email: formData["email"],
+      password: formData["password"],
     };
+
+    console.log(userData);
     try {
       const response = await registerUser(userData);
       console.log(response);
@@ -40,55 +58,56 @@ const Register = () => {
   };
 
   return (
-    <div>
+    <>
       <h1>Register</h1>
-      <form onSubmit={handleFormSubmit}>
-        {error && <div className="alert alert-danger">{error}</div>}
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input
+      <Form onSubmit={handleFormSubmit}>
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            name="name"
             type="text"
-            className="form-control"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            placeholder="Enter your name"
+            onChange={handleChange}
           />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email address</label>
-          <input
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            name="email"
             type="email"
-            className="form-control"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter email"
+            onChange={handleChange}
           />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            name="password"
             type="password"
-            className="form-control"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            onChange={handleChange}
           />
-        </div>
-        <div className="form-group">
-          <label htmlFor="confirm-password">Confirm Password</label>
-          <input
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+          <Form.Label>Confirm password</Form.Label>
+          <Form.Control
+            name="confirmPassword"
             type="password"
-            className="form-control"
-            id="confirm-password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
+            placeholder="Password"
+            onChange={handleChange}
           />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Register
-        </button>
-      </form>
-    </div>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
   );
 };
 
