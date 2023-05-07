eb8c2eba29501a36cf8712e0671e022c7110d096
Amartya Sen <amartyasen00077@gmail.com>
2023-05-07 05:45:20 +0530

    adds login api with jwt auth

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 // @route POST api/users/register
 // @desc Register a new user
@@ -35,6 +36,42 @@ router.post("/register", async (req, res) => {
     res.status(201).json({ message: "User registered succesfully" });
   } catch (e) {
     console.error(e.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+// @route POST /api/users/login
+// @desc login a user
+// @access Public
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // check if credentials correct
+    const user = await User.findOne({ email });
+    if (!user) {
+      console.error("Came when trying to find user with email");
+      return res
+        .status(401)
+        .json({ error: [{ message: "Invalid credentials" }] });
+    }
+
+    // Hash password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      console.error("Came when trying to match password");
+      return res
+        .status(401)
+        .json({ error: [{ message: "Invalid credentials" }] });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ token });
+  } catch (error) {
+    console.error(error);
     res.status(500).send("Internal Server Error");
   }
 });
